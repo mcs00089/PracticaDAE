06484a150eedbdd34f321ba167f919021c5493d0
Manuel Cámara Serrano <mcs00089@red.ujaen.es>
2025-10-08 15:32:48 +0200

    Primer commit

---
+++ src/main/java/es/ujaen/dae/incidenciasUrbanas/app/IncidenciasUrbanas.java
@@ -0,0 +1,18 @@
+
+package es.ujaen.dae.incidenciasUrbanas.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ *
+ * @author mcs00089
+ */
+@SpringBootApplication(scanBasePackages="es.ujaen.dae.incidenciasUrbanas.servicios")
+@EnableScheduling
+public class IncidenciasUrbanas {
+    public static void main(String[] args) {
+        SpringApplication.run(IncidenciasUrbanas.class);
+    }
+}
